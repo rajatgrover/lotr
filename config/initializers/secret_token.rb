70f23bf7f73930f6e38fc0006d52f8719f946810
Rajat Grover <rajat.grover@infibeam.net>
2014-11-22 15:44:23 +0530

    Initial Commit: rails new lotr && git init && git add .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lotr::Application.config.secret_token = '732b43ebe2bc11bfa22834a27f3e93409266de9ba953cc9d504bd8d295a6ae82269c93272a284a1d49d5fdc7f228e7a5975b21d22ed6137c3e9430c671df3022'
